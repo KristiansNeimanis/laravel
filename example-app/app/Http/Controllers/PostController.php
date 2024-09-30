c71039461795f27d3f8ab1fb921928fc3fee3c08
Kristians <you@example.com>
2024-09-30 15:40:33 +0300

    user can see all posts

--- example-app/app/Http/Controllers/PostController.php
+++ example-app/app/Http/Controllers/PostController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        //metode, kas atgriez visus post ierakstus
+        $posts = Post::all();
+        // \Log::debug($posts);
+        return view('posts.index', ['allposts' => $posts]);
+    }
 }
